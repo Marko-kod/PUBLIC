4f7d8025e91ac2648b1b8c2c9c4337b1f8083a0e
Marko-kod <Marko.Korenic@outlook.com>
2025-01-25 19:34:05 +0100

    update 1933

--- ODOO_addons/edi_provider/controllers/edi_dictionary.py
+++ ODOO_addons/edi_provider/controllers/edi_dictionary.py
@@ -2,7 +2,6 @@ from odoo import http
 from odoo.http import request
 
 class EdiDictionaryController(http.Controller):
-
     @http.route('/edi/dictionary', auth='public', website=True)
-    def edi_dictionary(self, **kw):
+    def edi_dictionary(self, **kwargs):
         return request.render('edi_provider.edi_dictionary_template')
